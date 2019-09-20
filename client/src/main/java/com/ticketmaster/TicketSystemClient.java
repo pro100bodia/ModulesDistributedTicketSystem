fe01be9cc8d1fafafceb8e216a1a1d65c8c694f5
Bohdan_Pukhalskyi <bohdanpukhalsky@ukr.net>
2019-09-20 13:06:55 +0300

    simplified TicketSystemClient`s method signature

--- client/src/main/java/com/ticketmaster/TicketSystemClient.java
+++ client/src/main/java/com/ticketmaster/TicketSystemClient.java
@@ -1,10 +1,15 @@
 package com.ticketmaster;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UriTemplate;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TicketSystemClient {
 
@@ -15,8 +20,15 @@ public class TicketSystemClient {
         this.restTemplate = restTemplate;
     }
 
-    public UserDto exchange(HttpHeaders httpHeaders, UserDto body, String returnedUrl){
-        HttpEntity<UserDto> entity = new HttpEntity<>(body, httpHeaders);
-        return restTemplate.exchange(returnedUrl, HttpMethod.GET, entity, UserDto.class).getBody();
+    public ResponseEntity<UserDto> getUser(String username){
+        String template = "http://localhost:8080/api/users/{username}";
+        UriTemplate uriTemplate = new UriTemplate(template);
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+
+        UriBuilder builder = UriComponentsBuilder.fromPath(template);
+        URI uri = builder.build(params);
+
+        return restTemplate.getForEntity(uri, UserDto.class);
     }
 }
