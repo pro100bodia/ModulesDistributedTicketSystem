744d87ccdf97ff1769c59e6e04fc0ac98d5a9590
Bohdan_Pukhalskyi <bohdanpukhalsky@ukr.net>
2019-09-20 14:33:03 +0300

    build restTemplate in bean initialize method

--- client/src/main/java/com/ticketmaster/configuration/RestConfig.java
+++ client/src/main/java/com/ticketmaster/configuration/RestConfig.java
@@ -1,20 +1,21 @@
 package com.ticketmaster.configuration;
 
 import com.ticketmaster.TicketSystemClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
 @EnableAutoConfiguration
+@Configuration
 public class RestConfig {
-    @Autowired
-    private RestTemplate restTemplate;
-
     @Bean
-    public TicketSystemClient restTemplate() {
+    public TicketSystemClient ticketSystemClient() {
+        RestTemplate restTemplate = new RestTemplateBuilder()
+                .basicAuthentication("serhiilytka", "serhiilytka")
+                .build();
+
         return new TicketSystemClient(restTemplate);
     }
 }
