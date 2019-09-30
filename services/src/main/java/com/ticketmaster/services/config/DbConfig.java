e1a8086668f3e86ed52b9918c961f736b27daea2
Bohdan_Pukhalskyi <bohdanpukhalsky@ukr.net>
2019-09-30 12:34:38 +0300

    add separate datasources

---
+++ services/src/main/java/com/ticketmaster/services/config/DbConfig.java
@@ -0,0 +1,37 @@
+package com.ticketmaster.services.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+    @Bean
+    public DataSource h2DataSource() {
+        EmbeddedDatabaseBuilder edb = new EmbeddedDatabaseBuilder();
+        edb.setType(EmbeddedDatabaseType.H2).setName("testdb")
+                .addDefaultScripts();
+
+        return edb.build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(mySqlDataSource());
+    }
+
+    private DataSource mySqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/ticketmaster");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+}
