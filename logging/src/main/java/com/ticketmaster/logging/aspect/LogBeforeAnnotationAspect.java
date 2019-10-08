128ff5b323af9c43e7e60d9c724d481e60c5f348
Bohdan_Pukhalskyi <bohdanpukhalsky@ukr.net>
2019-10-08 17:59:39 +0300

    configured logger for both stdout and file

--- logging/src/main/java/com/ticketmaster/logging/aspect/LogBeforeAnnotationAspect.java
+++ logging/src/main/java/com/ticketmaster/logging/aspect/LogBeforeAnnotationAspect.java
@@ -1,23 +1,22 @@
 package com.ticketmaster.logging.aspect;
 
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Aspect
 @Component
 public class LogBeforeAnnotationAspect {
-//    private static final Logger LOG = Logger.getLogger(LogBeforeAnnotationAspect.class);
-
-//    @Before("@annotation(com.ticketmaster.logging.annotation.LogBefore)")
-//    public void logBefore(JoinPoint joinPoint){
-////        LOG.info(String.format("%s received arguments: %s", joinPoint.getSignature(), joinPoint.getArgs()));
-//        System.out.println(String.format("###############################################%s received arguments: %s", joinPoint.getSignature(), Arrays.toString(joinPoint.getArgs())));
-//    }
+    private static final Logger LOG = Logger.getLogger(LogBeforeAnnotationAspect.class);
 
     @Before("@annotation(com.ticketmaster.logging.annotation.LogBefore)")
-    public void logBefore() {
-        System.out.println("################################Log Before");
+    public void logBefore(JoinPoint joinPoint) {
+        LOG.info("method occured");
+        LOG.info(String.format("%s received arguments: %s", joinPoint.getSignature()/*.toLongString()*/,
+                Arrays.toString(joinPoint.getArgs())));
     }
-
 }
--- logging/src/test/java/com/ticketmaster/logging/aspect/LogBeforeAnnotationAspectTest.java
+++ logging/src/test/java/com/ticketmaster/logging/aspect/LogBeforeAnnotationAspectTest.java
@@ -1,7 +1,7 @@
 package com.ticketmaster.logging.aspect;
 
+import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -13,15 +13,25 @@ import static org.mockito.Mockito.verify;
 @RunWith(MockitoJUnitRunner.class)
 public class LogBeforeAnnotationAspectTest {
     @Mock
-    private JoinPoint JoinPoint;
+    private JoinPoint joinPoint;
+
+    @Mock
+    private Logger logger;
 
     private LogBeforeAnnotationAspect aspect = new LogBeforeAnnotationAspect();
 
-    @Ignore
     @Test
-    public void shouldIntercept() throws Throwable {
-        aspect.logBefore();
+    public void shouldIntercept() {
+//        Signature signature = mock(Signature.class);
+//        when(joinPoint.getSignature()).thenReturn(signature);
+//        when(signature.toLongString()).thenReturn("Test signature");
+//        Object [] args = mock(Object[].class);
+//        when(joinPoint.getArgs()).thenReturn(args);
+//        when(Arrays.toString(args)).thenReturn("[test arg]");
 
-        verify(JoinPoint, times(1)).getSignature();
+        aspect.logBefore(joinPoint);
+
+        verify(joinPoint, times(1)).getSignature();
+//        verify(logger, times(1)).info(anyString());
     }
 }
